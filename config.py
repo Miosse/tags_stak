4d5c2679929aab242ef2f72e9596b6abea417fdc
Sebastien Toquet <seb@imac-de-sebastien.home>
2018-10-30 19:33:53 +0100

    ajout de config dans le commit

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 MODELS_DIRECTORY_PATH = basedir + '/prog_tags/data/MODEL/'
 
+DATA_DIRECTORY_PATH = basedir + '/prog_tags/data/'
+
 # SQLALCHEMY_TRACK_MODIFICATIONS = True
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
